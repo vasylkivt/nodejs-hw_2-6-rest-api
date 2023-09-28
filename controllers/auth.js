56bb6cd21845e208a5e7f378bb0cf6e387453b7e
vasylkivt <vasylkiv.t@gmail.com>
2023-09-28 14:11:53 +0300

    add avatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,33 @@
 const jwt = require("jsonwebtoken");
+const Jimp = require("jimp");
 const bcrypt = require("bcrypt");
 const { HttpError } = require("../utils");
+const fs = require("fs/promises");
 const User = require("../models/user");
+const gravatar = require("gravatar");
+const path = require("path");
 
 const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res, next) => {
   const { password, email } = req.body;
   const hashPassword = await bcrypt.hash(password, 10);
 
+  const avatarURL = gravatar.url(email);
+
   try {
     const newUser = await User.create({
       email,
       password: hashPassword,
+      avatarURL,
     });
 
-    res
-      .status(201)
-      .json({ email: newUser.email, subscription: newUser.subscription });
+    res.status(201).json({
+      email: newUser.email,
+      subscription: newUser.subscription,
+    });
   } catch (error) {
     if (error.name === "MongoServerError" && error.code === 11000) {
       next(new HttpError({ message: "Email in use", status: 409 }));
@@ -77,6 +87,33 @@ const subscription = async (req, res, next) => {
   }
 };
 
+const avatar = async (req, res, next) => {
+  const { _id } = req.user;
+  try {
+    const { path: tempUpload, originalname } = req.file;
+
+    const fileName = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, fileName);
+
+    const img = await Jimp.read(tempUpload);
+    await img
+      .autocrop()
+      .cover(
+        250,
+        250,
+        Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(tempUpload);
+
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", fileName);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.json({ avatarURL });
+  } catch (error) {
+    next(error);
+  }
+};
+
 const current = async (req, res, next) => {
   const { email, subscription } = req.user;
   res.json({ email, subscription });
@@ -93,4 +130,4 @@ const logout = async (req, res, next) => {
   }
 };
 
-module.exports = { register, login, current, logout, subscription };
+module.exports = { register, login, current, logout, subscription, avatar };
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,5 +2,12 @@ const isValidId = require("./isValidId");
 const isOwnerContact = require("./isOwnerContact");
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
-module.exports = { isValidId, validateBody, authenticate, isOwnerContact };
+module.exports = {
+  isValidId,
+  validateBody,
+  authenticate,
+  isOwnerContact,
+  upload,
+};
